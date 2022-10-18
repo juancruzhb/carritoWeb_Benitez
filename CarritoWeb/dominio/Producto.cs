f5e46eff349c6039ef3ee9a23c0a18332c63cff6
Juan Cruz <Juan Cruz@JUANCRUZW>
2022-10-18 17:35:09 -0300

    Se agrega master, navbar, bootstrap y comienza con el dominio

---
+++ CarritoWeb/dominio/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+        public string RutaImagen { get; set; }
+        public bool Activo { get; set; }
+    }
+}
