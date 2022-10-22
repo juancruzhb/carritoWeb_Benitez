987a56336266f4a81b10029d4c829d88884d1d71
Juan Cruz HB <Juan Cruz HB@JUANCRUZHB>
2022-10-22 18:04:52 -0300

    Se agregaron los metodos que suma a la cantidad si el articulo ya se encuentra en el carrito

--- CarritoWeb/Carrito/Carrito.aspx
+++ CarritoWeb/Carrito/Carrito.aspx
@@ -1,18 +1,25 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true"  CodeBehind="Carrito.aspx.cs" Inherits="Carrito.Carrito" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Carrito.aspx.cs" Inherits="Carrito.Carrito" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server" >
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
     <h2>Detalle de productos</h2>
     <div>
         <a href="Home.aspx">Seguir comprando</a>
         <hr />
-        <asp:GridView runat="server" ID="dgCarrito" CssClass ="table" AutoGenerateColumns ="false">
+        <asp:GridView runat="server" ID="dgCarrito" CssClass="table" AutoGenerateColumns="false">
             <Columns>
-                <asp:BoundField HeaderText ="Producto" DataField ="oArticulo.Nombre" />
-                <asp:BoundField HeaderText ="Descripcion" DataField ="oArticulo.Descripcion" />
-                <asp:BoundField HeaderText ="Producto" DataField ="oArticulo.Nombre" />
-                <asp:BoundField HeaderText ="Cantidad" DataField ="Cantidad" />
+                <asp:BoundField HeaderText="Producto" DataField="oArticulo.Nombre" />
+                <asp:BoundField HeaderText="Descripcion" DataField="oArticulo.Descripcion" />
+                <asp:BoundField HeaderText="Marca" DataField="oArticulo.oMarca.Descripcion" />
+                <asp:BoundField HeaderText="Cantidad" DataField="Cantidad" />
+                <asp:BoundField HeaderText="Precio" DataField="oArticulo.Precio" />
+                <asp:CommandField ShowDeleteButton="true" ButtonType="Button" />
+
             </Columns>
         </asp:GridView>
+        <div class="container">
+            <asp:Button Text="Finaliza compra" CssClass="btn btn-primary" runat="server" />
+        </div>
     </div>
 </asp:Content>
--- CarritoWeb/Carrito/Carrito.aspx.cs
+++ CarritoWeb/Carrito/Carrito.aspx.cs
@@ -17,31 +17,61 @@ namespace Carrito
         protected void Page_Load(object sender, EventArgs e)
         {
 
-
-            Articulo articulo = new Articulo();
             ArticuloNegocio negocio = new ArticuloNegocio();
             Carro nuevoCarro = new Carro();
+
+            List<Carro> carritoCompra = new List<Carro>();
+
             if (!string.IsNullOrEmpty(Request.QueryString["IdArticulo"]))
             {
-                nuevoCarro = carroPorId(int.Parse(Request.QueryString["IdArticulo"]));
-
-                List<Carro> carritoCompra = new List<Carro>();
+                int id = int.Parse(Request.QueryString["IdArticulo"]);
+                nuevoCarro = carroPorId(id);
                 //Get previous values
                 if (Session["data"] != null)
+                {
                     carritoCompra = (List<Carro>)Session["data"];
-                //Add new one
-                carritoCompra.Add(nuevoCarro);
-                //Save updated list
+                }
+
+                if(estaEnCarrito(nuevoCarro, carritoCompra))
+                {
+                    foreach (var item in carritoCompra.Where(x => x.oArticulo.IdArticulo == id))
+                        item.Cantidad++;
+                }
+                else
+                {
+                    carritoCompra.Add(nuevoCarro);
+                }
                 Session["data"] = carritoCompra;
 
                 dgCarrito.DataSource = carritoCompra;
                 dgCarrito.DataBind();
             }
+            if (Session["data"] != null)
+            {
             
+                carritoCompra = (List<Carro>)Session["data"];
+
+            }
+
+            Session["data"] = carritoCompra;
+
+            dgCarrito.DataSource = carritoCompra;
+            dgCarrito.DataBind();
+
 
         }
 
+        private Articulo articuloPorId(int id)
+        {
+            Articulo aux = new Articulo();
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            List<Articulo> articulos = new List<Articulo>();
+            articulos = negocio.listar();
 
+            aux = articulos.Find(x => x.IdArticulo == id);
+
+            return aux;
+        }
         private Carro carroPorId(int id)
         {
             Articulo aux = new Articulo();
@@ -58,6 +88,19 @@ namespace Carrito
             };
 
             return carro;
+        }
+
+        private bool estaEnCarrito(Carro carro, List<Carro> lista )
+        {
+            foreach (var item in lista)
+            {
+                if (lista.Any(x => x.oArticulo.IdArticulo == carro.oArticulo.IdArticulo))
+                {
+                    return true;
+                }
+                
+            }
+            return false;
         }
     }
 }
